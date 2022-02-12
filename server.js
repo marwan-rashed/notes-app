3db2cccd54fc8e9b10ba6d365ed2e674708c7a98
Marwan Rashed <marwanrashed300@gmail.com>
2022-02-12 21:14:45 +0200

    Add app structure

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-import express, { json } from 'express';
+import express from 'express';
+import notes from './routes/notes.js';
+import note from './routes/note.js';
 
 const app = express()
 const PORT = 8000;
+
+app.use(express.json());
+app.use('/api/notes', notes);
+app.use('/api/note', note);
+
+app.get(
+    '/',
+    (req, res) => {
+        res.json('Server running peacefully on port ' + PORT + ' ...');
+    }
+);
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT} ...`);
