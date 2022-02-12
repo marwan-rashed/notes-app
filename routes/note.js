59773ae436b59daaa6f15e5a1abeb3ae4e8f907a
Marwan Rashed <marwanrashed300@gmail.com>
2022-02-12 22:04:27 +0200

    Edit routes

--- routes/note.js
+++ routes/note.js
@@ -1,17 +1,21 @@
 import express from "express";
+import NoteController from "../controllers/note.js";
 
 const router = express();
 
 router.get(
     '/:noteId',
+    NoteController.getNote,
 );
 
 router.put(
     '/:noteId',
+    NoteController.editNote,
 );
 
 router.delete(
     '/:noteId',
+    NoteController.deleteNote,
 );
 
 export default router;
--- routes/notes.js
+++ routes/notes.js
@@ -1,13 +1,16 @@
 import express from "express";
+import NotesController from "../controllers/notes.js";
 
 const router = express();
 
 router.post(
     '/',
+    NotesController.createNote,
 );
 
 router.get(
     '/',
+    NotesController.getNotes,
 );
 
 export default router;
